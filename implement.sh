ef00c6134f32f009dd0dcc9ee21e802431cb22ce
Reorx <novoreorx@gmail.com>
2021-06-30 15:50:38 +0800

    refactor implement.sh after 9 years of time

--- implement.sh
+++ implement.sh
@@ -13,57 +13,144 @@
 
 # Global variables
 # Change this varible to choose the targets you want
-ALL_DOTFILES=( zsh vim git python moc wget tmux )
-IMPL_DOTFILES=( zsh vim git python wget tmux )
+IMPL_DOTFILES=( zsh git vim nvim python tmux misc bin )
 LINESHIFT="  "
 INITED_FILE=".inited"
 
-# Determine system
-# Supported systems: mac, ubuntu
-if [ "$(uname)" == "Darwin" ]; then
-    OS="mac"
-else
-    # release_info=$(cat /etc/*-release)
-    # if $release_info match 'ubuntu'
-    OS="ubuntu"
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
 
-    # OS="unknown"
-fi
-
-# TODO colorful print
-
-
-# Functions
-function ln2home() {
-    SOURCE="$PWD/$1"
-    if [ "$2" ]; then
-        TARGET="$HOME/$2"
-    else
-        TARGET="$HOME/.$1"
-    fi
-    if [ -e "$TARGET" ]; then
-        if [ $FORCE_SET ]; then
-            echo "$TARGET exists, force override it"
-            BACKUP="$HOME/.$1.dotfilebak"
-            echo "Backup to $BACKUP"
-            # To ensure BACKUP is no exist, or may cause link chain recurive
-            if [ -e "$BACKUP" ]; then
-                rm -rf $BACKUP
-            fi
-            mv $TARGET $BACKUP
-        else
-            echo "Unlinked file $TARGET exists, no force, skip"
-            return 0
-        fi
-    elif [ -h "$TARGET" ]; then
-        echo "$TARGET is a broken link, rm it"
-        rm -f $TARGET
-    fi
-
-    echo "Create link $TARGET -> $SOURCE"
-    ln -s $SOURCE $TARGET
+function paint() {
+    echo -e "${!1}${2}${NOCOLOR}"
 }
 
+# Determine system
+v_uname="$(uname)"
+if [ "$v_uname" == "Darwin" ]; then
+    OS="mac"
+else
+    OS="${v_uname,,}"
+fi
+
+
+# Logics
+
+function set_dotfiles() {
+    for i in ${IMPL_DOTFILES[@]}; do
+        paint GREEN "Setting $i's dotfiles"
+        eval "impl_$i"
+    done
+}
+
+function impl_zsh() {
+    ln2home oh-my-zsh
+    ln2home oh-my-zsh-custom
+    ln2home pure .zsh-pure
+    ln2home z
+    # ln2home zshenv
+    ln2home zshrc
+    ln2home zshrc_fzf
+    ln2home zshrc_$OS .zshrc_os
+}
+
+function impl_vim() {
+    ln2home vim
+    ln2home vimrc
+}
+
+function impl_nvim() {
+    mkdir -p ~/.config/nvim
+    create_link "$PWD/nvim/init.vim" ~/.config/nvim/init.vim
+    ln2home nvim
+    ln2home nvimrc
+}
+
+function impl_git() {
+    ln2home gitconfig
+    ln2home gitignore_global
+}
+
+function impl_python() {
+    ln2home pythonrc.py
+}
+
+function impl_tmux {
+    ln2home tmux.conf
+    local tpm=~/.tmux/plugins/tpm
+    if [ -e "$tpm" ]; then
+        paint BLUE "$tpm exists, skip"
+    else
+        echo "Download git repo to $tpm"
+        mkdir -p "$(dirname $tpm)"
+        git clone https://github.com/tmux-plugins/tpm "$tpm"
+    fi
+}
+
+function impl_bin {
+    local source_dir="$PWD/bin"
+    local target_dir=~/.local/bin
+    mkdir -p "$target_dir"
+    for cmd in $(ls "$source_dir"); do
+        create_link "$source_dir/$cmd" "$target_dir/$cmd"
+    done
+}
+
+function impl_misc {
+    ln2home ignore
+    ln2home bashrc
+}
+
+# Utils
+
+function ln2home() {
+    local source="$PWD/$1"
+    if [ "$2" ]; then
+        local target="$HOME/$2"
+    else
+        local target="$HOME/.$1"
+    fi
+    create_link $source $target
+}
+
+function create_link() {
+    local source="$1"
+    local target="$2"
+    if [ -e "$target" ]; then
+        if [ -L "$target" ]; then
+            paint BLUE "$target link exists, skip"
+            return 0
+        fi
+
+        paint ORANGE "$target exists, force override it"
+        local BACKUP=$(dirname "$target")/$(basename "$target").dotfilebak
+        echo "Backup to $BACKUP"
+        # To prevent creating recursive link
+        if [ -e "$BACKUP" ]; then
+            rm -rf $BACKUP
+        fi
+        mv $target $BACKUP
+    elif [ -h "$target" ]; then
+        paint ORANGE "$target is a broken link, rm it"
+        rm -f $target
+    fi
+
+    echo "Create link $target -> $source"
+    ln -s $source $target
+}
 
 function installed() {
     if type -P $1 &>/dev/null; then
@@ -73,155 +160,41 @@ function installed() {
     fi
 }
 
-
-function install() {
-    if [ "$OS" == "mac" ]; then
-        if ! type "brew" > /dev/null; then
-            echo "You must have brew installed on your mac to \
-                  be able to install $1 by command"
-            exit
-        fi
-    elif [ "$OS" == "ubuntu" ]; then
-        # release_info=$(cat /etc/*-release)
-        # if $release_info match 'ubuntu'
-        if ! type "apt-get" > /dev/null; then
-            echo "No apt-get? WTF!"
-            exit
-        fi
-        sudo apt-get install $1
-    else
-        echo "Sorry, does not support linux dist other than ubuntu yet"
+function require_installed() {
+    if [ ! $(installed "$1") ]; then
+        paint RED '$1 must be installed !'
+        exit 1
     fi
 }
-
-
-function install_if_not_exist() {
-    if [ ! $(installed $1) ]; then
-        install $1
-    fi
-}
-
-
-function impl_zsh() {
-    install_if_not_exist zsh
-    ln2home shell-colors
-    ln2home oh-my-zsh
-    ln2home oh-my-zsh-custom
-    ln2home zshrc
-    ln2home zshrc_fzf
-    ln2home zshrc_$OS .zshrc_os
-    ln2home z
-}
-
-
-function impl_vim() {
-    install_if_not_exist vim
-    install_if_not_exist ctags
-    ln2home vim
-    ln2home vimrc
-}
-
-
-function impl_git() {
-    ln2home gitconfig
-    ln2home gitignore_global
-}
-
-
-function impl_python() {
-    ln2home pythonrc.py
-}
-
-
-function impl_moc() {
-    install_if_not_exist moc
-    ln2home moc
-}
-
-
-function impl_wget() {
-    install_if_not_exist wget
-    ln2home wgetrc
-}
-
-function impl_tmux {
-    ln2home tmux.conf
-}
-
-
-function conf_zsh() {
-    local user="$(whoami)"
-    read -p "Do you want to set to this user $user(yourself) [yY]?" -n 1
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo
-    elif [ $REPLY ]; then
-        echo
-        read -p "input username: " user
-    fi
-    sudo usermod -s /bin/zsh $user
-}
-
-
-function conf_vim() {
-    #(
-    #vim +BundleInstall +qall
-    #)
-    vim -c "execute \"BundleInstall\" | q | q"
-}
-
-
-function update_git() {
-    git pull
-    git submodule init
-    git submodule update
-}
-
 
 function show_help() {
     cat <<EOF
-Usage: implement.sh [-i | -s name | -c name | -h]
+Usage: implement.sh [-i | -f | -s name | -h]
        require option arguments
        non-option arguments is not needed
 Options:
 -i : Initialize everything, for virgin system.
--u : Update all dotfiles implementations.
+-f : Initialize by force.
 -s : Set dotfiles specially.
--c : Configure with <name>.
 -h : Show help information.
 EOF
 }
 
-
-function check_repos() {
+function check_git_submodules() {
     if [ ! -e $INITED_FILE ]; then
-        echo "init repos first"
-        update_git
+        paint GREEN "Init submodules first"
+        git submodule update --init
         touch $INITED_FILE
-        # If this is done before command `update_git`,
-        # there will be no need to redo update,
-        # set a flag for later use.
-        FIRST_TIME="true"
     fi
 }
 
 
-function set_dotfiles() {
-    for i in ${IMPL_DOTFILES[@]}; do
-        echo "Setting $i's dotfiles"
-        eval "impl_$i"
-    done
-}
-
-
-# Preparations
-if [ ! $(installed git) ]; then
-    echo 'git must be installed !'
-    exit
-fi
-
-cd "$(dirname "$0")"
-
 # Main
+
+require_installed git
+
+# cd to where implment.sh located in
+cd "$(dirname "$0")"
 
 while getopts ":hTiuxs:c:" opt; do
     case $opt in
@@ -229,67 +202,27 @@ while getopts ":hTiuxs:c:" opt; do
             show_help
             exit
             ;;
-        T)
-            echo "self test" >&2
-            exit
-            ;;
         i)
-            check_repos
+            check_git_submodules
 
-            echo "Initialize"
-
-            echo "Step 1. Set dotfiles"
             set_dotfiles
 
-            read -p "Do you want to configure zsh to your default shell [yY]?" -n 1
-            if [[ $REPLY =~ ^[Yy]$ || ! $REPLY ]]; then
-                conf_zsh
-            fi
-
-            read -p "Do you want to configure vim and its bundles [yY]?" -n 1
-            if [[ $REPLY =~ ^[Yy]$ || ! $REPLY ]]; then
-                conf_vim
-            fi
-
-            echo "\nAll done!"
-            exit
-            ;;
-        u)
-            FORCE_SET="true"
-
-            echo "Update dotfiles"
-            set_dotfiles
-
+            paint GREEN "\nAll done!"
             exit
             ;;
         s)
-            echo "Set $OPTARG"
-
-            FORCE_SET="true"
+            paint GREEN "Set $OPTARG"
 
             eval "impl_$OPTARG"
 
             exit
             ;;
-        c)
-            echo "Configure $OPTARG"
-
-            eval "conf_$OPTARG"
-
-            exit
-            ;;
-        x)
-            echo "Delete all backup files"
-            rm -f $HOME/.*.dotfilebak
-
-            exit
-            ;;
         \?)
-            echo "Invalid option: -$OPTARG"
+            paint RED "Invalid option: -$OPTARG"
             exit 1
             ;;
         :)
-            echo "Option -$OPTARG requires an argument."
+            paint RED "Option -$OPTARG requires an argument."
             exit 1
             ;;
     esac
