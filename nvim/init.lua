3889c2e0b81236d9c543f868f997ab6b9c0038d1
Reorx <novoreorx@gmail.com>
2025-07-08 18:21:28 +0800

    nvim: configure telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,23 @@ require('config.cmp')
 --require('config.lsp')
 require('config.lsp_old')
 
+function vim.getVisualSelection()
+  local current_clipboard_content = vim.fn.getreg('"')
+
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
+
+  vim.fn.setreg('"', current_clipboard_content)
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
+
 vim.cmd[[
   source ~/.config/nvim/nvimrc
   let $MYVIMRC='~/.nvim/nvimrc'
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,5 +5,19 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
+    config = function()
+      vim.cmd([[
+        noremap <c-/> :Telescope find_files<cr>
+      ]])
+
+      local builtin = require('telescope.builtin')
+      local opts = { noremap = true, silent = true }
+
+      vim.keymap.set('n', '<leader>f', builtin.grep_string, opts)
+      vim.keymap.set('v', '<leader>f', function()
+        local text = vim.getVisualSelection()
+        builtin.current_buffer_fuzzy_find({ default_text = text })
+      end, opts)
+    end,
   },
 }
