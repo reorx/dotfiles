3aaa10da5c8049d9437316cea69363ded1f01830
Reorx <novoreorx@gmail.com>
2025-07-10 23:29:48 +0800

    nvim: add render-markdown

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -54,4 +54,23 @@ return {
     ft = 'yaml',
   },
 
+  -- Markdown
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ft = { 'markdown' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      file_types = { 'markdown' },
+      heading = {
+        enabled = false,
+      },
+      code = {
+        disable_background = true,
+        style = 'normal',
+        border = 'none',
+      },
+    },
+  }
 }
