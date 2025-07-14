183b0b7c7eaff622dd6bb266db109eb285014663
Reorx <novoreorx@gmail.com>
2025-07-14 16:04:58 +0800

    nvim: do not show path in dropbar

--- nvim/lua/plugins/common.lua
+++ nvim/lua/plugins/common.lua
@@ -34,13 +34,40 @@ local plugins = {
   },
   -- https://github.com/Bekaboo/dropbar.nvim
   {
-    'Bekaboo/dropbar.nvim',
+    'Bekaboo/dropbar.nvim',  -- The symbol breadcrumb plugin
     -- optional, but required for fuzzy finder support
     dependencies = {
       'nvim-telescope/telescope-fzf-native.nvim',
       build = 'make'
     },
     config = function()
+      local sources = require('dropbar.sources')
+      local utils = require('dropbar.utils')
+      require('dropbar').setup({
+        bar = {
+          sources = function(buf, _)
+            if vim.bo[buf].ft == 'markdown' then
+              return {
+                sources.path,
+                sources.markdown,
+              }
+            end
+            if vim.bo[buf].buftype == 'terminal' then
+              return {
+                sources.terminal,
+              }
+            end
+            -- for regular files, sources.path is excluded
+            return {
+              utils.source.fallback({
+                sources.lsp,
+                sources.treesitter,
+              }),
+            }
+          end,
+        },
+      })
+
       local dropbar_api = require('dropbar.api')
       vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
       vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
