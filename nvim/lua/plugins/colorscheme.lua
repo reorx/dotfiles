07f3722a42fb9fd25b0115d848b4be7ffad12dcc
Reorx <novoreorx@gmail.com>
2025-07-17 16:45:21 +0800

    nvim: add alternative colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
-return {
+local use_theme = 'catppuccin'
+--local use_theme = 'kanagawa'
+--local use_theme = 'rose-pine'
+
+local plugins = {
   {
     "catppuccin/nvim",
+    enabled = false,
     name = "catppuccin",
     priority = 1000,
     config = function()
@@ -12,6 +17,7 @@ return {
         no_bold = false, -- Force no bold
         -- No need to override color if enabling transparent_background
         --transparent_background = true,
+        -- HEX colors: https://catbbrew.com/design
         color_overrides = {
           mocha = {
             base = "#030303",
@@ -88,10 +94,66 @@ return {
     end,
   },
 
-  -- Other candidates
-  -- Plug 'rebelot/kanagawa.nvim'  " https://github.com/rebelot/kanagawa.nvim
-  -- Plug 'EdenEast/nightfox.nvim'  " https://github.com/EdenEast/nightfox.nvim
-  -- Plug 'oxfist/night-owl.nvim'  " https://github.com/oxfist/night-owl.nvim
+  {
+    -- https://github.com/rebelot/kanagawa.nvim
+    'rebelot/kanagawa.nvim',
+    name = 'kanagawa',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('kanagawa').setup({
+      })
+      vim.cmd('colorscheme kanagawa-dragon')
+    end,
+  },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('rose-pine').setup({
+      })
+      vim.cmd('colorscheme rose-pine')
+    end,
+  },
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('github-theme').setup({
+        -- ...
+      })
+
+      vim.cmd('colorscheme github_dark_default')
+    end,
+  },
+  {
+    -- https://github.com/oxfist/night-owl.nvim
+    "oxfist/night-owl.nvim",
+    name = 'night-owl',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      require("night-owl").setup()
+      vim.cmd.colorscheme("night-owl")
+    end,
+  },
   -- Plug 'bluz71/vim-moonfly-colors', { 'as': 'moonfly' }
-  -- Plug 'tanvirtin/monokai.nvim'  " https://github.com/tanvirtin/monokai.nvim
 }
+
+-- Only enables the one with the colorscheme name
+for _, plugin in ipairs(plugins) do
+  if plugin.name == use_theme then
+    plugin.enabled = true
+  end
+end
+
+return plugins
