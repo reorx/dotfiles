6d6f64a3815d3d9cb792b684723eb3097c3adee8
Reorx <novoreorx@gmail.com>
2025-07-17 16:53:40 +0800

    nvim: use treesitter-context instead of dropbar

--- nvim/lua/plugins/common.lua
+++ nvim/lua/plugins/common.lua
@@ -121,6 +121,7 @@ local plugins = {
   {
     -- https://github.com/Bekaboo/dropbar.nvim
     'Bekaboo/dropbar.nvim',  -- The symbol breadcrumb plugin
+    enabled = false,
     -- optional, but required for fuzzy finder support
     dependencies = {
       'nvim-telescope/telescope-fzf-native.nvim',
@@ -293,18 +294,6 @@ local plugins = {
     branch = 'master',
     lazy = false,
     build = ':TSUpdate',
-
-    dependencies = {
-      -- NOTE: use dropbar instead of nvim-treesitter-context for code context
-      --{
-      --  'nvim-treesitter/nvim-treesitter-context',
-      --  event = 'BufReadPost',
-      --  opts = function()
-      --    return { mode = 'cursor', max_lines = 2 }
-      --  end,
-      --},
-    },
-
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     opts = {
       ensure_installed = {
@@ -317,6 +306,30 @@ local plugins = {
         additional_vim_regex_highlighting = false,
       },
     },
+  },
+  {
+    -- NOTE: use either this one or dropbar, depending on your preference for winbar
+    'nvim-treesitter/nvim-treesitter-context',
+    enabled = true,
+    --event = 'BufReadPost',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require'treesitter-context'.setup{
+        enable = true,
+        multiwindow = false, -- Enable multiwindow support.
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      }
+    end,
   },
   {
     -- https://github.com/folke/todo-comments.nvim
